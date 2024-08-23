d10f39b2b806683b3553649a83a6aa136a3c0d31
Bharath Ramesh <bramesh.dev@gmail.com>
2024-08-22 20:56:09 -0700

    Update PATH and MANPATH
    * Update PATH and MANPATH based on what is generated by path_helper

--- dot_zsh/rc/path.zsh
+++ dot_zsh/rc/path.zsh
@@ -1,3 +1,15 @@
+# start PATH added by running /usr/libexec/path_helper
+export PATH="/usr/local/bin${PATH+:$PATH}";
+export PATH="/System/Cryptexes/App/usr/bin${PATH+:$PATH}";
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin${PATH+:$PATH}";
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin${PATH+:$PATH}";
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appinternal/bin${PATH+:$PATH}";
+# end
+export PATH="/Library/TeX/texbin${PATH+:$PATH}"
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+# start MANPATH added by running /usr/libexec/path_helper
+export MANPATH="/usr/share/man:/usr/local/share/man${MANPATH+:$MANPATH}";
+# end
+export MANPATH="/Library/TeX/Distributions/.DefaultTeX/Contents/Man${MANPATH+:$MANPATH}";
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
