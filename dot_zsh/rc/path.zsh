f7dce94dc35ae41dee34a6793438b8606e858b03
Bharath Ramesh <bramesh.dev@gmail.com>
2026-03-04 00:39:41 -0800

    Use path_helper to setup initial path

--- dot_zsh/rc/path.zsh
+++ dot_zsh/rc/path.zsh
@@ -1,17 +1,10 @@
-# start PATH added by running /usr/libexec/path_helper
-export PATH="/usr/local/bin${PATH+:$PATH}";
-export PATH="/System/Cryptexes/App/usr/bin${PATH+:$PATH}";
-export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin${PATH+:$PATH}";
-export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin${PATH+:$PATH}";
-export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appinternal/bin${PATH+:$PATH}";
-# end
-export PATH="/Library/TeX/texbin${PATH+:$PATH}"
+# initialize running /usr/libexec/path_helper
+eval "$(/usr/libexec/path_helper)"
+
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 [[ -d ${HOME}/go/bin ]] && export PATH="${HOME}/go/bin${PATH+:$PATH}";
 [[ -d ${HOME}/bin ]] && export PATH="${HOME}/bin${PATH+:$PATH}";
-# start MANPATH added by running /usr/libexec/path_helper
-export MANPATH="/usr/share/man:/usr/local/share/man${MANPATH+:$MANPATH}";
-# end
-export MANPATH="/Library/TeX/Distributions/.DefaultTeX/Contents/Man${MANPATH+:$MANPATH}";
+
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}";
+
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
